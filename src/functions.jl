562935252338ecb41fd61994a232d78429c7a69e
Kevin Bonham <kevbonham@gmail.com>
2019-04-29 14:21:25 -0400

    add function for non-unique files

--- src/functions.jl
+++ src/functions.jl
@@ -17,3 +17,32 @@ function resolve_sampleID(sid::Union{AbstractString,Symbol})
         return (sample=sample, subject=subject, timepoint=timepoint)
     end
 end
+
+"""
+Add batch info to `merged/` folders.
+
+    Example:
+        `batch001/metaplan2/merged/metaphlan2_taxonomic_profiles.tsv`
+        to
+        `batch001/metaplan2/merged/batch001_metaphlan2_taxonomic_profiles.tsv`
+"""
+function add_batch_info(folder)
+    batch = ""
+    for (root, dirs, files) in walkdir(folder)
+           if occursin(r"batch\d+$", root)
+               batch = match(r"(batch\d+)", root).captures[1]
+               @warn batch
+           end
+
+           if occursin(r"merged$", root)
+               for file in files
+                   if !occursin("batch", file)
+                       oldpath = joinpath(root, file)
+                       newpath = joinpath(root, "$(batch)_$file")
+                       @info "moving $oldpath to $newpath"
+                   end
+               end
+           end
+       end
+   end
+end
