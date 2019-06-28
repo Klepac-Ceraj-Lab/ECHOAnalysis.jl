f55c4d65ba5f8891daed00dd35658bd6717a2da6
Kevin Bonham, PhD <kevbonham@gmail.com>
2019-06-28 11:38:32 -0400

    more documenter prep

--- docs/make.jl
+++ docs/make.jl
@@ -7,19 +7,19 @@ makedocs(
     "Analysis" => [
         "01-data-sources.md",
         "02-metadata-merging.md",
-        "03-metadata-analysis.md",
-        "04-mgx-qc.md",
-        "05-mgx-taxonomic-profiles.md",
-        "06-omnibus-tests.md"
+        # "03-metadata-analysis.md",
+        # "04-mgx-qc.md",
+        # "05-mgx-taxonomic-profiles.md",
+        # "06-omnibus-tests.md"
         ],
     ],
     expandfirst=[
         "01-data-sources.md",
         "02-metadata-merging.md",
-        "03-metadata-analysis.md",
-        "04-mgx-qc.md",
-        "05-mgx-taxonomic-profiles.md",
-        "06-omnibus-tests.md"
+        # "03-metadata-analysis.md",
+        # "04-mgx-qc.md",
+        # "05-mgx-taxonomic-profiles.md",
+        # "06-omnibus-tests.md"
     ],
     format = Documenter.HTML(
             prettyurls = get(ENV, "CI", nothing) == "true"
